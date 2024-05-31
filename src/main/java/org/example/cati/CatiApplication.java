e8ca6c121a082b42234faaffa133b0887a6196a9
francescokrum <francescopk19@gmail.com>
2024-05-31 19:13:16 -0300

    (feat): upando o ambiente

---
+++ src/main/java/org/example/cati/CatiApplication.java
@@ -0,0 +1,13 @@
+package org.example.cati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/cati/CatiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
